d28efce39d7b2f31cbb94319e7694c4a2da89632
hibi <69405847+hibiii@users.noreply.github.com>
2023-10-16 18:03:07 -0300

    better pretty printing

--- format.lua
+++ format.lua
@@ -3,38 +3,82 @@ local Format = {}
 local Lexer = require './lexer'
 local TypeId = Lexer.TypeId
 
+local width = 80
+
+local widthBoundPrinter = function(str, prefix, options)
+    if not prefix then prefix = "" end
+    if options == nil then options = {} end
+    local out = prefix
+    local pad = (" "):rep(assert(utf8.len(prefix)))
+    for word in str:gmatch("(%g+)") do
+        if utf8.len(out) + utf8.len(word) + 1 < width then
+            if out == "" then
+                out = word
+            elseif out == prefix or out == pad then
+                out = out .. word
+            else
+                out = out .. " " .. word
+            end
+        else
+            print(out)
+            if options.prefix_once then
+                out = pad .. word
+            else
+                out = prefix .. word
+            end
+        end
+    end
+    if options.new_line then
+        print(out .. "\n")
+    else
+        print(out)
+    end
+end
+
 local printers = {
     ---@param text TextLine
     [TypeId.Text] = function(text)
-        print(text.content .. "\n")
+        widthBoundPrinter(text.content, nil, {new_line = true})
     end,
+
     ---@param link LinkLine
     [TypeId.Link] = function(link)
         if link.friendly_name then
-            print("-> " .. link.friendly_name)
+            widthBoundPrinter(link.friendly_name, "--> ", {prefix_once = true})
             print("   <" .. link.link .. "> \n")
         else
             print("-> <" .. link.link .. "> \n")
         end
     end,
-    [TypeId.PreToggle] = function()
-        print()
+
+    ---@param toggle PreformatToggle
+    [TypeId.PreToggle] = function(toggle)
+        if toggle.meta then
+            print ("    ~~~~ " .. toggle.meta .. " ~~~~")
+        else
+            print()
+        end
     end,
+
     ---@param line PreformatLine
     [TypeId.PreLine] = function(line)
         print("    " .. line.content)
     end,
+
     ---@param item ListItem
     [TypeId.ListItem] = function(item)
-        print(" • " .. item.content .. "\n")
+        widthBoundPrinter(item.content, " • ",
+            {new_line = true, prefix_once = true})
     end,
+
     ---@param quote QuoteLine
     [TypeId.Quote] = function (quote)
-        print(" | " .. quote.content .. "\n")
+        widthBoundPrinter(quote.content, " │ ", {new_line = true})
     end,
+
     ---@param heading HeadingLine
     [TypeId.Heading] = function (heading)
-        print("    " .. heading.content .. "\n")
+        widthBoundPrinter(heading.content, "    ", {new_line = true})
     end
 }
 
